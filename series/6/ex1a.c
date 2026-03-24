b5a197082c0412a83c73350e332177aed46f77b5
Dionys Kaloulis <generalhellothere55555@gmail.com>
2026-03-24 15:25:39 +0100

    Finished s6ex1a and s6ex1b

--- series/6/ex1a.c
+++ series/6/ex1a.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 
-int plus10(int a) {
-	a += 10;
-	printf("plus 10 = %d\n", a);
+int plus10(int a) { // function plus10 takes an integer a as argument
+	a += 10; // adds 10 to a, a is local to this function
+	printf("plus 10 = %d\n", a); // displays the value of the local a after adding 10
 	return a;
 }
 
 int main() {
 	int v = 5;
-	printf("v = %d\n", v);
-	plus10(plus10(v));
-	printf("v = %d\n", v);
+	printf("v = %d\n", v); // will print the initial value of v, which is 5
+	plus10(plus10(v)); // calls plus10(5), which will print 15, return 15, then calls plus10(15), which will print 25, return 25, but the return value is not useded or us
+	printf("v = %d\n", v); // prints the value of v which is still 5
 }
+
+// predicted output:
+//
+// v = 5
+// plus10 = 15
+// plus10 = 25
+// v = 5
--- series/6/ex1b.c
+++ series/6/ex1b.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 
-int* ptr_plus10(int *a) {
-	*a += 10;
-	printf("plus 10 = %d\n", *a);
-	return a;
+int* ptr_plus10(int *a) { // takes a pointer to an int and returns a pointer to an int
+	*a += 10; // adds 10 to the value of a (external variable)
+	printf("plus10 = %d\n", *a); // prints the the variable a after having added 10 to it
+	return a; // returns the pointer to the variable a
 }
 
 int main() {
-	int v = 5;
-	printf("v = %d\n", v);
-	ptr_plus10(ptr_plus10(&v));
-	printf("v = %d\n", v);
+	int v = 5; // declares an integer variable v and initializes it to 5
+	printf("v = %d\n", v); // prints the value of v
+	ptr_plus10(ptr_plus10(&v)); // calls ptr_plus10 with the pointer to v: &v, then returns the same pointer to v after having added 10 to v's value
+	printf("v = %d\n", v); // prints the value of v again, (added 10 twice -> +20)
 }
+
+// Predicted output:
+//
+// v = 5
+// plus10 = 15
+// plus10 = 25
+// v = 25
